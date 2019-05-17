e2078c399824687918802302cfe975dbf1bf1bb9
Sachin Panigrahi <sachinpanigrahi@hotmail.com>
2019-05-17 21:24:44 +0530

    added alamofire and moya (#1)

--- UnitTest/Controllers/LoginScreenViewController.swift
+++ UnitTest/Controllers/LoginScreenViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginScreenViewController.swift
 //  UnitTest
 //
 //  Created by Sachin Panigrahi on 17/05/19.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-
 }
-
