1bca4a8c49acc30a3cef99aaa0e0142edc8be968
Sachin Panigrahi <sachinpanigrahi@hotmail.com>
2019-05-18 20:09:42 +0530

    added layout constraints extension and some test cases

--- UnitTest/Controllers/LoginScreenViewController.swift
+++ UnitTest/Controllers/LoginScreenViewController.swift
@@ -8,10 +8,132 @@
 
 import UIKit
 
-class LoginScreenViewController: UIViewController {
+extension UIView {
+    func setAnchors(top : NSLayoutYAxisAnchor? = nil,bottom: NSLayoutYAxisAnchor? = nil,leading: NSLayoutXAxisAnchor? = nil,trailing: NSLayoutXAxisAnchor? = nil,constant : UIEdgeInsets?) {
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: (constant?.top)!).isActive = true
+        }
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: (constant?.left)!).isActive = true
+        }
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: (constant?.right)!).isActive = true
+        }
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: (constant?.bottom)!).isActive = true
+        }
+    }
+    
+    func setAnchorPosition(horizontal : NSLayoutXAxisAnchor? = nil, vertical : NSLayoutYAxisAnchor? = nil){
+        if let horizontal = horizontal {
+            self.centerXAnchor.constraint(equalTo: horizontal).isActive = true
+        }
+        
+        if let vertical = vertical {
+            self.centerYAnchor.constraint(equalTo: vertical).isActive = true
+        }
+    }
+    
+    func setAnchorSize(size : CGSize) {
+        if size.height != 0{
+            self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        if size.width != 0{
+            self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+    }
+}
 
+extension UILabel {
+    var preInitilize : String {
+        get {
+            return "just awesome"
+        }
+        set(newVal) {
+            self.translatesAutoresizingMaskIntoConstraints = false
+            self.text = newVal
+        }
+    }
+}
+
+class BaseLabel : UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.frame = frame
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    init(text : String) {
+        super.init(frame: .zero)
+        self.text = text
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class LoginScreenViewController: UIViewController {
+    
+    var simpleText = BaseLabel(text: "Hello world")
+    var alert = UIAlertController(title: "Error", message: "enter name", preferredStyle: .alert)
+    var actionString = ""
+    let lbl : UILabel = {
+        let label  = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello world"
+        label.font = .systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let fn : UITextField = {
+        let tf  = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.borderStyle = .roundedRect
+//        tf.text = "Hello world"
+        tf.placeholder = "enter first name"
+        tf.font = .systemFont(ofSize: 18)
+        return tf
+    }()
+    
+    let clickMeButtom : UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Clicked", for: .normal)
+        button.setTitle("Not Clicked", for: .selected)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.setTitleColor(UIColor.blue, for: .selected)
+        button.backgroundColor = UIColor.orange
+        button.isUserInteractionEnabled = true
+        button.addTarget(self, action: #selector(LoginScreenViewController.handleClick), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .cyan
+        
+        simpleText = BaseLabel(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+//        view.addSubview(simpleText)
+        view.addSubview(clickMeButtom)
+        view.addSubview(fn)
+        
+        view.addSubview(lbl)
+        
+        lbl.setAnchorPosition(horizontal: view.centerXAnchor, vertical: view.centerYAnchor)
+        clickMeButtom.setAnchors(top: view.topAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, constant: UIEdgeInsets.init(top: 100, left: 100, bottom: 0, right: -100))
+        fn.setAnchors(top: clickMeButtom.bottomAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, constant: UIEdgeInsets.init(top: 20, left: 40, bottom: 0, right: -40))
+        
+        clickMeButtom.setAnchorSize(size: CGSize.init(width: 0, height: 100))
+    }
+    
+    @objc func handleClick() {
+        if fn.text!.isEmpty {
+            DispatchQueue.main.async {
+                self.present(self.alert, animated: true) {
+                }
+            }
+        }
     }
 }
--- UnitTestTests/UnitTestTests.swift
+++ UnitTestTests/UnitTestTests.swift
@@ -11,24 +11,44 @@ import XCTest
 
 class UnitTestTests: XCTestCase {
     
+    var vc = LoginScreenViewController()
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+//        _ = vc.view
+//        vc.loadView()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    class FakeAlert : UIAlertAction {
+//        var alertTitle : String?
+//        var alertStyle : UIAlertAction.Style?
+//        var alertHandler : (UIAlertAction)->(Void)?
+//
+//        convenience init(title: String?, handler: ((UIAlertAction) -> Void)?, preferredStyle: UIAlertController.Style = .alert) {
+//            self.init()
+//            self.alertTitle = title
+//            self.alertHandler = handler!
+//        }
+//
+//        override init() {
+//            self.alertStyle = UIAlertAction.Style.default
+//            super.init()
+//        }
+//
+//        required init?(coder aDecoder: NSCoder) {
+//            fatalError("init(coder:) has not been implemented")
+//        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testLoginScreenClickMeButton() {
+        XCTAssertNotNil(vc)
+        let button = vc.clickMeButtom
+        XCTAssertNotNil(button)
+        var msgStr = ""
+        vc.handleClick()
+        
     }
-
 }
